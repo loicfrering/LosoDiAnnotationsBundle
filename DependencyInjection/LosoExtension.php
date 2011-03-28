9dd10b6e6bfc2d70a3d9a489e5f9331d12aa389e
Loïc Frering <loic.frering@gmail.com>
2011-03-29 00:54:09 +0200

    LosoExtension internal refactoring.

--- DependencyInjection/LosoExtension.php
+++ DependencyInjection/LosoExtension.php
@@ -5,61 +5,70 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use LoSo\LosoBundle\DependencyInjection\Loader\AnnotationLoader;
 
+/**
+ * LosoExtension for LosoBundle.
+ *
+ * @author Loïc Frering <loic.frering@gmail.com>
+ */
 class LosoExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        echo '<pre>';
-        print_r($configs);
-        echo '</pre>';
         $config = $configs[0];
-        $paths = array();
         if (isset($config['service_scan'])) {
             foreach ($config['service_scan'] as $scanName => $scanConfig) {
                 if (isset($scanConfig['dir']) && is_array($scanConfig['dir'])) {
-                    foreach ($scanConfig['dir'] as $dir) {
-                        if (is_dir($dir)) {
-                            $paths[] = $dir;
-                        } else {
-                            throw new \InvalidArgumentException('Invalid scan directory "%s".');
-                        }
-                    }
+                    $this->loadDirectory($scanConfig['dir'], $container);
                 } else {
-                    foreach ($container->getParameter('kernel.bundles') as $name => $class) {
-                        if ($scanName === $name) {
-                            $bundle = new \ReflectionClass($class);
-                            $bundleDir = dirname($bundle->getFilename());
-                            if (isset($scanConfig['base_namespace']) && is_array($scanConfig['base_namespace'])) {
-                                foreach ($scanConfig['base_namespace'] as $baseNamespace) {
-                                    $path = $bundleDir.'/'.$baseNamespace;
-                                    if (is_dir($path)) {
-                                        $paths[] = $path;
-                                    } else {
-                                        throw new \InvalidArgumentException('Invalid base namespace "%s" for bundle "%s".');
-                                    }
-                                }
-                            } else {
-                                $paths[] = $bundleDir;
-                            }
-                            break;
-                        }
-                    }
+                    $this->loadBundle($scanName, $scanConfig, $container);
                 }
             }
         }
+    }
 
-        echo '<pre>';
-        print_r($paths);
-        echo '</pre>';
-
-        foreach ($paths as $path) {
-            $this->loadPath($path, $container);
+    private function loadDirectories(array $directories, ContainerBuilder $container)
+    {
+        foreach ($directories as $dir) {
+            if (is_dir($dir)) {
+                $this->loadDir($dir, $container);
+            } else {
+                throw new \InvalidArgumentException(sprintf('Invalid scan directory "%s".', $dir));
+            }
         }
     }
 
-    private function loadPath($path, ContainerBuilder $container)
+    private function loadBundle($bundle, array $config, ContainerBuilder $container)
+    {
+        $isValidBundle = false;
+        foreach ($container->getParameter('kernel.bundles') as $bundleName => $bundleClass) {
+            if ($bundle === $bundleName) {
+                $bundle = new \ReflectionClass($bundleClass);
+                $bundleDir = dirname($bundle->getFilename());
+                if (isset($config['base_namespace']) && is_array($config['base_namespace'])) {
+                    foreach ($config['base_namespace'] as $baseNamespace) {
+                        $dir = $bundleDir.'/'.$baseNamespace;
+                        if (is_dir($dir)) {
+                            $this->loadDir($dir, $container);
+                        } else {
+                            throw new \InvalidArgumentException(sprintf('Invalid base namespace "%s" for bundle "%s".', $baseNamespace, $bundleName));
+                        }
+                    }
+                } else {
+                    $this->loadDir($bundleDir, $container);
+                }
+                $isValidBundle = true;
+                break;
+            }
+        }
+
+        if (!$isValidBundle) {
+            throw new \InvalidArgumentException(sprintf('Invalid bundle "%s".', $bundle));
+        }
+    }
+
+    private function loadDir($dir, ContainerBuilder $container)
     {
         $loader = new AnnotationLoader($container);
-        return $loader->load($path);
+        return $loader->load($dir);
     }
 }
